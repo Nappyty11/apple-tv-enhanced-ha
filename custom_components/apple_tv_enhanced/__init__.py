b4134f16e8bf11d2609cfbd9e04637b0aa25765b
Nappyty11 <tylerjvisuals@gmail.com>
2026-06-01 20:41:44 -0700

    Update __init__.py

--- custom_components/apple_tv_enhanced/__init__.py
+++ custom_components/apple_tv_enhanced/__init__.py
@@ -1,1 +1,53 @@
+"""Apple TV Enhanced integration."""
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
+
+DOMAIN = "apple_tv_enhanced"
+PLATFORMS = ["media_player"]
+
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up integration."""
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Set up from config entry."""
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    device_registry = dr.async_get(hass)
+
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        identifiers={(DOMAIN, entry.entry_id)},
+        name="Apple TV Enhanced",
+        manufacturer="Nappyty11",
+        model="Enhanced Apple TV Controller",
+        sw_version="0.0.1",
+        suggested_area="Living Room",
+    )
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Unload entry."""
+
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
