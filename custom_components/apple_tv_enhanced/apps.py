63396e2a70666f4d64b552b9f3b4e795ad247325
Nappyty11 <tylerjvisuals@gmail.com>
2026-06-01 20:44:21 -0700

    Create apps.py

---
+++ custom_components/apple_tv_enhanced/apps.py
@@ -0,0 +1,15 @@
+"""Apple TV app bundle IDs."""
+
+APP_IDS = {
+    "Apple TV": "com.apple.TVWatchList",
+    "Netflix": "com.netflix.Netflix",
+    "YouTube": "com.google.ios.youtube",
+    "Hulu": "com.hulu.plus",
+    "HBO Max": "com.wbd.stream",
+    "Amazon Prime": "com.amazon.aiv.AIVApp",
+    "Peacock": "com.peacocktv.peacock",
+    "Disney Plus": "com.disney.disneyplus",
+    "Twitch": "tv.twitch",
+    "Crunchyroll": "com.crunchyroll.iphone",
+    "Calendar by Dashbd": "com.benoitzohar.Calendar",
+}
